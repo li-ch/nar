98a89b48f9291009bb12a058a07cd1d7d09ad516
Li Chen 陈 力 <li-ch@users.noreply.github.com>
2026-03-12 17:23:25 +0800

    Revert "iter7: kTarget=12ms kCeil=70ms wider ramp"
    This reverts commit 4811c43daf44c462bcd372966b574a3a80671a6a.

--- topics/cc/src/cc-algo.cc
+++ topics/cc/src/cc-algo.cc
@@ -67,7 +67,7 @@ TcpAutoCC::IncreaseWindow(Ptr<TcpSocketState> tcb, uint32_t segmentsAcked)
     {
         double qd_ms = (lastRtt - m_baseRtt).GetMilliSeconds();
         const double kTarget = 12.0; // start scaling at 12 ms of queuing
-        const double kCeil   = 70.0; // stop growing at 70 ms of queuing
+        const double kCeil   = 50.0; // stop growing at 50 ms of queuing
         if (qd_ms >= kCeil)
         {
             scale = 0.0;
